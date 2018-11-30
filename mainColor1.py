928eceab90bc4a5bb3f9877f57128885d895b605
Siyuan Li <shulisiyuan@163.com>
2018-11-30 19:30:50 +0800

    添加了部分注释

--- mainColor1.py
+++ mainColor1.py
@@ -1,34 +1,30 @@
-import numpy as np
 from skimage import io
 from sklearn.cluster import KMeans
+import numpy as np
 import warnings
 
 warnings.filterwarnings('ignore')
 
 img_file = 'city.jpg'
 
-# k in k-means
+# k-means中的k值，即选择几个中心点
 k = 5
 
-# get img
+# 读图片
 img = io.imread(img_file)
-# reshape 2D img to 1D
+# 转换数据维度
 img_ori_shape = img.shape
 img1 = img.reshape((img_ori_shape[0] * img_ori_shape[1], img_ori_shape[2]))
 img_shape = img1.shape
-print('img size:', img_ori_shape)
 
-# get pixels counts
-n_pixels = img_shape[0]
-
-# get channels count
+# 获取图片色彩层数
 n_channels = img_shape[1]
 
 estimator = KMeans(n_clusters=k, max_iter=4000, init='k-means++', n_init=50)  # 构造聚类器
 estimator.fit(img1)  # 聚类
 centroids = estimator.cluster_centers_  # 获取聚类中心
 
-
+# 使用算法跑出的中心点，生成一个矩阵，为数据可视化做准备
 result = []
 result_width = 200
 result_height_per_center = 80
@@ -37,4 +33,5 @@ for center_index in range(k):
 result = np.array(result)
 result = result.reshape((result_height_per_center * k, result_width, n_channels))
 
+# 保存图片
 io.imsave(img_file + '.result.bmp', result)
